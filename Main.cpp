0aa8c781fe7c2520148ba4271fd990f325b7d933
LucaCursa <127750423+LucaCursa@users.noreply.github.com>
2024-02-29 10:33:51 +0100

    Main
    Main with linear combination of different numbers

--- Main.cpp
+++ Main.cpp
@@ -1,27 +1,12 @@
-/** \file main.c
-	\brief Numeric integration of a polynomial
-	\author Paolo Gastaldo
-
-	first version of this software
-*/
-
-/*! \mainpage
- *
- * \section intro_sec Introduction
- *
- * This is a software in C language for calculating numerical integration by adopting different techniques
- *
- * \section install_sec Installation
- *
- *
- *
- * etc...
- */
-
 #include <stdio.h> 
 #include <stdlib.h> 
 #include <string.h>
 #include "mathf.h" //dichiaro le mie funzioni
+
+#define NUM_POLYNOMIALS 4 // Numero di polinomi
+#define NUM_XMIN 2 // Numero di valori di xmin
+#define NUM_XMAX 2 // Numero di valori di xmax
+#define NUM_INTERVALS 3 // Numero di valori di intervals
 
 /*! \brief the main function
 
@@ -30,48 +15,61 @@
 
 */
 int main() {
-
 	/* setting all the parameters */
-	float fcoeff[4] = { -10.0,1.0,0.0,2.0 };	/* the polynomial */
-	float xmin = 0.0; 						/* the integration range */
-	float xmax = 5.0;
-	int intervals = 1000; 					/* number of equally spaced intervals */
-
+	float fcoeff[NUM_POLYNOMIALS][5] = {
+		{-10.0, 1.0, 0.0, 2.0, 7.0},
+		{2.0, 0.0, -1.0, 3.0, 0.0},
+		{0.0, 0.0, 1.0, -5.0, 0.0}
+	}; // Gli array dei coefficienti dei polinomi
+	float xmin[NUM_XMIN] = { 0.0, -2.0 }; //Valori Xmin
+	float xmax[NUM_XMAX] = { 5.0, 3.0 }; //Valori di Xmax
+	int intervals[NUM_INTERVALS] = { 500, 1000, 2000 }; //Intervalli
 
 	float integ1, integ2;
+	int i, j, k, l;
 
-	int i;
-	float in = xmin;
-	float gap = (xmax - xmin) / (float)intervals;
-	float* fvalues = NULL;
+	for (i = 0; i < NUM_POLYNOMIALS; i++) 
+	{
+		for (j = 0; j < NUM_XMIN; j++) 
+		{
+			for (k = 0; k < NUM_XMAX; k++) 
+			{
+				for (l = 0; l < NUM_INTERVALS; l++) 
+				{
+					float in = xmin[j];
+					float gap = (xmax[k] - xmin[j]) / (float)intervals[l];
+					float* fvalues = NULL;
 
-	/* this array will store the (steps+1) values of the polynomial that are delimiting the equally spaced intervals*/
-	fvalues = (float*)malloc(sizeof(float) * (intervals + 1));
-	if (fvalues == NULL) {
-		printf("\nERROR: cannot allocate memory\n");
-		exit(-1);
+					fvalues = (float*)malloc(sizeof(float) * (intervals[l] + 1));
+					if (fvalues == NULL)
+					{
+						printf("\nERROR: cannot allocate memory\n");
+						exit(-1);
+					}
+
+					for (int m = 0; m <= intervals[l]; m++)
+					{
+						fvalues[m] = Polynomial(fcoeff[i], 4, in);
+						in += gap;
+					}
+
+					integ1 = 0.;
+					integ2 = 0.;
+
+					//Rectangular rule
+					Rectangular(fvalues, intervals[l] + 1, gap, &integ1, &integ2);
+					printf("\nPolynomial %d, Xmin: %f, Xmax: %f, Intervals: %d - Rectangular rule - The integral is in the interval: [%f,%f]\n", i, xmin[j], xmax[k], intervals[l], integ1, integ2);
+
+					//Trapezoidal rule
+					integ1 = Trapezoidal(fvalues, intervals[l] + 1, gap);
+					printf("Polynomial %d, Xmin: %f, Xmax: %f, Intervals: %d - Trapezoidal rule - The integral is: %f\n", i, xmin[j], xmax[k], intervals[l], integ1);
+
+					if (fvalues != NULL)
+						free(fvalues);
+				}
+			}
+		}
 	}
-
-	/* to get the value of the polynomial at the different points that are delimiting the intervals */
-	for (i = 0; i <= intervals; i++) {
-		fvalues[i] = Polynomial(fcoeff, 4, in);
-		in += gap;
-	}
-
-	integ1 = 0.;
-	integ2 = 0.;
-
-	/* to compute the integral according to the rectangular rule */
-	Rectangular(fvalues, intervals + 1, gap, &integ1, &integ2);
-	printf("\nRectangular rule - The integral between %f and %f is in the interval: [%f,%f]\n", xmin, xmax, integ1, integ2);
-
-	/* to compute the integral according to the trapezoidal rule */
-	integ1 = Trapezoidal(fvalues, intervals + 1, gap);
-	printf("\nTrapezoidal rule - The integral between %f and %f is : %f\n", xmin, xmax, integ1);
-
-	/* deallocation */
-	if (fvalues != NULL)
-		free(fvalues);
 
 	return 0;
 }
